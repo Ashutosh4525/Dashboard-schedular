ea1cce783be597da0c3d0367808227844a9dbc62
Ashutosh4525 <ashutoshpal47@gmail.com>
2025-12-15 23:54:39 +0530

    routes

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,16 +1,17 @@
 'use client'
+import Link from "next/link";
+import axios from "axios";
+import { Formik } from "formik";
+import * as Yup from 'yup';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 
 export default function Login() {
+  const router = useRouter();
+  const[formError,setformError]=useState("");
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-900">
-        <body class="h-full">
-        ```
-      */}
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
@@ -22,20 +23,62 @@ export default function Login() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" method="POST" className="space-y-6">
+           <Formik
+                initialValues={{ username: '', password: '' }}
+                validate={(values) => {
+                    const errors = {};
+                    if (!values.username) {
+                    errors.username = 'Username is Required';
+                    }
+                    if(!values.password){
+                        errors.password='Password is Required'
+                    }
+                    return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                // alert(JSON.stringify(values, null, 2));
+                axios
+                .post("https://dummyjson.com/auth/login",values)
+                .then((response)=>{
+                    console.log(response.data.accessToken); 
+                    sessionStorage.setItem("token",response.data.accessToken);
+                    router.push("/admin");
+                })
+                .catch((error)=>{
+                    // console.error(error.response.data.message);
+                    setformError(error.response.data.message);
+                })
+                .finally(() => setSubmitting(false));
+                setSubmitting(false);
+                }, 400);
+              }}
+            >
+         {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+       }) => (
+        <form onSubmit={handleSubmit} className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
-                Email address
+              <label htmlFor="username" className="block text-sm/6 font-medium text-gray-100">
+                User name
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  autoComplete="email"
+                  id="username"
+                  name="username"
+                  type="text"
+                  onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.username}
                   className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                 />
+                <p className="text-sm text-red-700">{errors.username && touched.username && errors.username}</p>
               </div>
             </div>
 
@@ -55,22 +98,27 @@ export default function Login() {
                   id="password"
                   name="password"
                   type="password"
-                  required
-                  autoComplete="current-password"
+                 onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
                   className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                 />
+                 <p className="text-sm text-red-700">{errors.password && touched.password && errors.password}</p>
               </div>
             </div>
 
+           
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500" disabled={isSubmitting}
               >
                 Sign in
               </button>
             </div>
           </form>
+       )}
+      </Formik>
 
           <p className="mt-10 text-center text-sm/6 text-gray-400">
             Not a member?{' '}
